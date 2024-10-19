2078c345c4b84c84e6931fe31c8d54cb24116421
Enzo Bustamante <enzobustamante895@gmail.com>
2024-10-19 01:15:58 -0300

    feat: add styles

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,5 +1,14 @@
-export default function Header() {
-  <header className="flex justify-center">
-    <p>ppapapap</p>
-  </header>;
+import React from "react";
+
+function Header() {
+  return (
+    <header className="flex flex-col items-center  font-[family-name:var(--font-geist-mono)] leading-6">
+      <h1 className="text-4xl uppercase font-bold">hello I&apos;m</h1>
+      <h2>Enzo Bustamante</h2>
+      <h3>IES 9-021</h3>
+      <h4>Student</h4>
+    </header>
+  );
 }
+
+export default Header;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import SplineNext from "@splinetool/react-spline/next";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <main className="w-full h-screen flex flex-col justify-center items-center bg-tra">
-      <SplineNext scene="https://prod.spline.design/avqpoNifVn3shW7b/scene.splinecode" />
+    <main className="w-full ">
+      <div>
+        <Header />
+      </div>
     </main>
   );
 }
