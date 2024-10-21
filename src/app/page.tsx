92d4c25ca31847381e321f4f2ccb3bf3c74c421a
Enzo Bustamante <enzobustamante895@gmail.com>
2024-10-20 23:52:02 -0300

    fix: css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <main className="w-full ">
-      <div>
+    <main className="w-full h-full">
+      <div className="flex flex-col items-center  h-screen justify-center ">
         <Header />
       </div>
     </main>
